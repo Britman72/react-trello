c060a3e2db3c816524dd8531c6382c10bbd4983a
Britman72 <alougher@gmail.com>
2023-12-06 17:17:41 -0500

    Update Base.js 2

--- src/styles/Base.js
+++ src/styles/Base.js
@@ -165,7 +165,7 @@ export const CardWrapper = styled.article`
   position: relative;
   padding: 10px;
   cursor: pointer;
-  max-width: 250px;
+  max-width: 290px;
   margin-bottom: 7px;
   min-width: 230px;
 `
